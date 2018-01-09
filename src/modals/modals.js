3a20aaa65e240d59298659bb1d1a9325d6e066c0
Antoine Moreaux <moreaux.antoinne@gmail.com>
2018-01-09 23:30:39 +0100

    fix bug add mask with current document symbol

--- src/modals/modals.js
+++ src/modals/modals.js
@@ -92,7 +92,7 @@ function importModal(context) {
     let colorLib = this.colorLibsMenuParams.selectedItem()
     result.colorLib = (colorLib) ? this.colorLibsMenuParams.representedObject() : null
 
-    if(!result.color || !result.colorLib)result.withMask = false
+    if(!result.color)result.withMask = false
   } else if (result.withMask) {
     result.colorPicker = this.colorPickerColor || MSColor.blackColor()
   }
