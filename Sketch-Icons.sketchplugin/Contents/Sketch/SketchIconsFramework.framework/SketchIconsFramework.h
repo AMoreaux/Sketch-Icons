79372513a9861270457e43e8c2461fc9602d6cf5
Antoine Moreaux <moreaux.antoine@gmail.com>
2018-05-18 12:13:39 +0200

    add sources

---
+++ Sketch-Icons.sketchplugin/Contents/Sketch/SketchIconsFramework.framework/SketchIconsFramework.h
@@ -0,0 +1,19 @@
+//
+//  SketchIconsFramework.h
+//  SketchIconsFramework
+//
+//  Created by Antoine on 19/12/2017.
+//  Copyright © 2017 Antoine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchIconsFramework.
+FOUNDATION_EXPORT double SketchIconsFrameworkVersionNumber;
+
+//! Project version string for SketchIconsFramework.
+FOUNDATION_EXPORT const unsigned char SketchIconsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchIconsFramework/PublicHeader.h>
+
+
